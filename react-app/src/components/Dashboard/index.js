f4bef064a91a3d0b4074335b1d38185a67c42854
Alana <alana.lapoint@gmail.com>
2021-04-16 12:23:03 -0400

    friends

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -3,20 +3,47 @@ import {Link, Redirect} from 'react-router-dom'
 import {useSelector, useDispatch} from 'react-redux'
 import MyPlants from './MyPlants'
 import {showProfiles} from '../../store/profile'
+import {showFollows} from '../../store/user'
 import DashNav from './DashNav'
 import './Dashboard.css'
 
 const Dashboard = () => {
- 
+   // const [loaded, setLoaded] = useState(false)
    const me = useSelector((state) => state.session.user)
    const dispatch = useDispatch()
+   let follows = useSelector((state) => state.user.follows)
+   if (follows) {
+      follows = follows['follow']
+      // setLoaded(true)
+   }
 
    useEffect(() => {
       dispatch(showProfiles())
+      // setLoaded(true)
    },[])
+   
+   // useEffect(() => {
+      dispatch(showFollows(me.id))
+      // },[])
 
-   if (me) {
-      return (
+   const getUser = async(id) => {
+      const res = await fetch(`/api/users/${id}`)
+      if (res.ok) {
+         const data = await res.json()
+         return data;
+      }
+   }
+
+   let following = [];
+ 
+   if (follows) {
+          follows.forEach(async(follow) => {
+         let friend = await getUser(follow.friend_id)
+         await following.push(friend) 
+      })
+      console.log(following, 'following')
+   }
+   if (me) { return (
       <div className='dashboard'>
          <div>
             <DashNav me={me}/>   
@@ -32,17 +59,29 @@ const Dashboard = () => {
                <p>{me.bio}</p>
                <p className='zone-info'>{`USDA Zone: ${me.zone}`}</p>
             </div>
+            <div className='following'>
+               {following && following.map((friend) => (
+                  <div key={friend.id}>
+                     {console.log('friend')}
+                     <Link to={`/users/${friend.id}`}>
+                        <img src={friend.avatar} 
+                        alt='avatar' 
+                        style={{height: '100px', width: '100px', borderRadius: '50%'}}/>
+                        <h5>{friend.name}</h5>
+                     </Link>
+                  </div>
+               ))}
+            </div>
             <div className='users-plants-container'>
                <MyPlants />
             </div>
          </div>
       </div>
-      )
+      ) 
    } else {
-      return (
-      <h1>Loading...</h1>
-      )
-   }}
+      return <h1>...loading</h1>
+   }
+}
 
 
 
--- react-app/src/store/user.js
+++ react-app/src/store/user.js
@@ -1,9 +1,48 @@
 const GET_USER = 'user/GET_USER'
+const GET_FOLLOWS  = 'user/GET_FOLLOWS'
+const ADD_FOLLOW = 'user/ADD_FOLLOW'
 
 const getUser = (user) => ({
    type: GET_USER,
    user
 })
+
+const getFollows = (follows) => ({
+   type: GET_FOLLOWS,
+   follows
+})
+
+const addFollow = (follow) => ({
+   type: ADD_FOLLOW,
+   follow
+})
+
+export const showFollows = (id) => async(dispatch) => {
+   const res = await fetch(`/api/users/${id}/follows`)
+   if (res.ok) {
+      const data = await res.json()
+      dispatch(getFollows(data))
+      return data
+   }
+} 
+
+export const newFollow = (follow) => async(dispatch) => {
+   const {user_id, friend_id} = follow;
+   const res = await fetch(`/api/users/${user_id}/follows`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+         user_id,
+         friend_id
+      })
+   });
+   if (res.ok) {
+      const data = await res.json()
+      dispatch(addFollow(data))
+      return data
+   }
+}
+
 
 export const showUser = (id) => async(dispatch) => {
    const res = await fetch(`/api/users/${id}`)
@@ -22,6 +61,14 @@ export default function reducer(state={}, action) {
       case GET_USER:
          newState= {...state}
          newState['user'] = action.user;
+         return newState;
+      case ADD_FOLLOW:
+         newState = {...state}
+         newState['follow'] = action.follow;
+         return newState;
+      case GET_FOLLOWS:
+         newState = {...state}
+         newState['follows'] = action.follows;
          return newState;
       default: return state;
    }
